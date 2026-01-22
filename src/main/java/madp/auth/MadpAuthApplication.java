2eb5eeea1cf3d65dd4fde1d1dc83e534a99a8a35
Lian08 <lian080408@gmail.com>
2026-01-22 13:55:26 +0900

    Initial Commit

---
+++ src/main/java/madp/auth/MadpAuthApplication.java
@@ -0,0 +1,13 @@
+package madp.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadpAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadpAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/madp/auth/MadpAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package madp.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadpAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
