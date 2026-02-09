b1843eeffb3d0787bfc1af21522ca7e636967169
Lian08 <lian080408@gmail.com>
2026-02-09 17:19:45 +0900

    feat(auth-svc) :: src/main/java/madp/auth/domain/infrastructure/jwt/constants/JwtConstants.java 추가

---
+++ src/main/java/madp/auth/domain/infrastructure/jwt/constants/JwtConstants.java
@@ -0,0 +1,8 @@
+package madp.auth.domain.infrastructure.jwt.constants;
+
+public class JwtConstants {
+    public static final String CLAIM_MADE_BY_KEY = "made_by";
+    public static final String CLAIM_MADE_BY_VALUE = "madp";
+    public static final String CLAIM_ROLE_KEY = "role";
+    public static final String REFRESH_TOKEN_COOKIE_HEADER = "refresh_token";
+}
