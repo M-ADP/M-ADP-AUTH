6141e456a8cd3e88c9f03a953f2c213c1aa796cc
Lian08 <lian080408@gmail.com>
2026-02-09 17:19:45 +0900

    feat(auth-svc) :: src/main/java/madp/auth/domain/infrastructure/security/vo/MadpOAuth2User.java 추가

---
+++ src/main/java/madp/auth/domain/infrastructure/security/vo/MadpOAuth2User.java
@@ -0,0 +1,47 @@
+package madp.auth.domain.infrastructure.security.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import madp.auth.domain.domain.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Getter
+public class MadpOAuth2User implements OAuth2User {
+
+    private final Long userId;
+    private final String username;
+    private final Role role;
+    private final Map<String, Object> attributes;
+    
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.getValue()));
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return username;
+    }
+
+    @Builder
+    public MadpOAuth2User(Long userId, String username, Role role, Map<String, Object> attributes) {
+        this.userId = userId;
+        this.username = username;
+        this.role = role;
+        this.attributes = attributes;
+    }
+
+}
