dd85932c38cf67de22a2614fca0f7b80994ed30a
Lian08 <lian080408@gmail.com>
2026-03-03 07:58:36 +0900

    feat(auth) :: 경로 변경

--- src/main/java/madp/auth/domain/domain/repository/OAuth2SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package madp.auth.domain.domain.repository;
-
-import madp.auth.domain.domain.entity.OAuth2SessionEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OAuth2SessionRepository extends CrudRepository<OAuth2SessionEntity, String> {
-}
--- src/main/java/madp/auth/global/configuration/SecurityConfiguration.java
+++ src/main/java/madp/auth/global/configuration/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import madp.auth.global.enums.Role;
 import madp.auth.domain.infrastructure.security.handler.MadpOAuth2FailureHandler;
 import madp.auth.domain.infrastructure.security.handler.MadpOAuth2SuccessHandler;
-import madp.auth.domain.infrastructure.security.resolver.MadpOAuth2AuthorizationRequestResolver;
 import madp.auth.domain.infrastructure.security.service.MadpOAuth2UserService;
 import madp.auth.global.properties.WebProperties;
 import madp.auth.global.security.filter.MadpUserInfoExtractorFilter;
@@ -29,7 +28,6 @@ public class SecurityConfiguration {
     private final MadpOAuth2UserService madpOAuth2UserService;
     private final MadpOAuth2SuccessHandler madpOAuth2SuccessHandler;
     private final MadpOAuth2FailureHandler madpOAuth2FailureHandler;
-    private final MadpOAuth2AuthorizationRequestResolver madpOAuth2AuthorizationRequestResolver;
 
     @Bean
     public PathMatcher pathMatcher() {return new AntPathMatcher();}
@@ -50,10 +48,10 @@ public class SecurityConfiguration {
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage(webProperties.getFrontEndUrl() + "/login")
                         .authorizationEndpoint(authorization -> authorization
-                                .authorizationRequestResolver(madpOAuth2AuthorizationRequestResolver)
+                                .baseUri("/auth/oauth2/authorization")
                         )
                         .redirectionEndpoint(redirection -> redirection
-                                .baseUri("/oauth2/callback/*")
+                                .baseUri("/auth/oauth2/callback/*")
                         )
                         .successHandler(madpOAuth2SuccessHandler)
                         .failureHandler(madpOAuth2FailureHandler)
