a1a9476f9542f33aa07a936833abdb1308d7c22b
Lian08 <lian080408@gmail.com>
2026-03-05 23:07:39 +0900

    feat(auth) :: OAuth2Session Redis로 변경

--- src/main/java/madp/auth/global/configuration/InternalServiceCommunicationConfiguration.java
+++ src/main/java/madp/auth/global/configuration/InternalServiceCommunicationConfiguration.java
@@ -1,6 +1,8 @@
 package madp.auth.global.configuration;
 
 import feign.RequestInterceptor;
+import lombok.RequiredArgsConstructor;
+import madp.auth.domain.domain.repository.OAuth2SessionRepository;
 import madp.auth.global.infrastructure.internal.InternalRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +10,12 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(FeignClientConfiguration.class)
+@RequiredArgsConstructor
 public class InternalServiceCommunicationConfiguration {
+    private final OAuth2SessionRepository oAuth2SessionRepository;
 
     @Bean
     public RequestInterceptor requestInterceptor() {
-        return new InternalRequestInterceptor();
+        return new InternalRequestInterceptor(oAuth2SessionRepository);
     }
 }
